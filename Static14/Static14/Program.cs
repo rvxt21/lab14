5086b43f349fbabacb70cc0f18a7b158aa9cadc9
rvxt21 <78904227+rvxt21@users.noreply.github.com>
2021-11-02 12:51:15 +0200

    1

---
+++ Static14/Static14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Static14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobile a = new Automobile("bmw");
+            a.GetInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
